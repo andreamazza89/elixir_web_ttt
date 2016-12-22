096d30427f54362bc496ada2a86bfe10061f1ff0
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-12-22 14:11:48 +0100

    WIP, first two integration test, game landing page

--- lib/tic_tac_toe/web/router.ex
+++ lib/tic_tac_toe/web/router.ex
@@ -5,8 +5,12 @@ defmodule TicTacToe.Web.Router do
   plug :match
   plug :dispatch
 
-  get ("/") do
-    conn |> put_resp_content_type("text/plain") |> send_resp(200, "yo")
+  get ("/tictactoe/play") do
+    #game_id = somehow get or set game id in session
+    #game_state = TicTacToe.Web.SessionManager.new_or_existing_game()
+    #response_body = TicTacToe.Web.Views.render_game(game_state)
+    response_body = "<p>It is x's turn, please pick a move</p> <form><button type=\"submit\" formaction=\"/tictactoe/moves/0\"></form>"
+    conn |> put_resp_content_type("html") |> send_resp(200, response_body)
   end
 
 end
--- test/tic_tac_toe/unit/game_state_test.exs
+++ test/tic_tac_toe/unit/game_state_test.exs
@@ -6,39 +6,34 @@ defmodule GameStateTest do
   @second_cell 1
 
   test "provides an id for the game state" do
-    game = "I am a game"
-    initial_state_id = GameState.start_game(game)
-    assert is_pid(initial_state_id)
+    game = "this is a game double"
+    state_id = GameState.start_game(game)
+
+    assert is_pid(state_id)
   end
 
   test "initialises the state to the game given" do
-    game = "I am a game"
-    initial_state_id = GameState.start_game(game)
-    initial_game_state = GameState.get_state(initial_state_id)
+    game = "This is a game double"
+    state_id = GameState.start_game(game)
+    initial_game_state = GameState.get_state(state_id)
     
-    assert initial_game_state === "I am a game"
+    assert initial_game_state === "This is a game double"
   end
 
   test "updates a game with the given move, first cell" do
-    player_one = %Player.Human{mark: :x}
-    player_two = %Player.Human{mark: :o}
-    game = %Game{players: {player_one, player_two}}
-    initial_state_id = GameState.start_game(game)
+    state_id = GameState.start_game(create_game_with_human_players(:x, :o))
 
-    GameState.add_move(initial_state_id, @first_cell)
-    updated_game = GameState.get_state(initial_state_id) 
+    GameState.add_move(state_id, @first_cell)
+    updated_game = GameState.get_state(state_id) 
 
     assert get_cell_at(@first_cell, updated_game) === :x
   end
 
   test "updates a game with the given move, second cell" do
-    player_one = %Player.Human{mark: :o}
-    player_two = %Player.Human{mark: :x}
-    game = %Game{players: {player_one, player_two}}
-    initial_state_id = GameState.start_game(game)
+    state_id = GameState.start_game(create_game_with_human_players(:o, :x))
 
-    GameState.add_move(initial_state_id, @second_cell)
-    updated_game = GameState.get_state(initial_state_id) 
+    GameState.add_move(state_id, @second_cell)
+    updated_game = GameState.get_state(state_id) 
 
     assert get_cell_at(@second_cell, updated_game) === :o
   end
