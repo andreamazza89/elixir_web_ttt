b81f9cc70d0ceccdfaa6b36b5802d6d76c0617fd
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-12-23 12:08:38 +0100

    Refactor router and game presenter

--- lib/tic_tac_toe/web/game_presenter.ex
+++ lib/tic_tac_toe/web/game_presenter.ex
@@ -4,6 +4,7 @@ defmodule TicTacToe.Web.GamePresenter do
     board_width = Board.width(game.board)
     rows = game.board.cells |> Enum.with_index |> Enum.chunk(board_width)
     {current_player, _next_player} = game.players
+
     EEx.eval_file("templates/game.eex", [current_player: current_player, rows: rows])
   end
 
