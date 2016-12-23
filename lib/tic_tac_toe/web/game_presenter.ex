28a085c0cfda7000bd1dbdfe7b1e106a6b7db220
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-12-23 11:02:28 +0100

    Refactor and style front-end

--- lib/tic_tac_toe/web/game_presenter.ex
+++ lib/tic_tac_toe/web/game_presenter.ex
@@ -1,9 +1,10 @@
 defmodule TicTacToe.Web.GamePresenter do
 
   def render_game(game) do
-    cells = game.board.cells
+    board_width = Board.width(game.board)
+    rows = game.board.cells |> Enum.with_index |> Enum.chunk(board_width)
     {current_player, _next_player} = game.players
-    EEx.eval_file("templates/game.eex", [current_player: current_player, cells: cells])
+    EEx.eval_file("templates/game.eex", [current_player: current_player, rows: rows])
   end
 
 end
